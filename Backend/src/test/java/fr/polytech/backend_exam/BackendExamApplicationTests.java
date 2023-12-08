5b633aa23900455cecdf26ece31ebc20dcad20f9
Joffrey <joffrey.mischlerps@gmail.com>
2023-12-08 09:19:39 +0100

    first commit

---
+++ Backend/src/test/java/fr/polytech/backend_exam/BackendExamApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.polytech.backend_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
